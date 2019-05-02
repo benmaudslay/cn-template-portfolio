fff87a8a36738423836717691eedd65105ea0599
Ben <ben@Bens-MBP.lan>
2019-05-02 14:13:02 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Get all "navbar-burger" elements
+//   const tabs = Array.prototype.slice.call(
+//     document.getElementsByClassName("menuTab"),
+//     0
+//   )
+
+//   for (let i = 0; i < tabs.length; i++) {
+//     tabs[i].addEventListener("click", function() {
+//       let current = document.getElementsByClassName("is-active")
+//       if (current.length > 0) {
+//         current[0].className = current[0].className.replace(" is-active", "")
+//       }
+//       this.className += " is-active"
+//     })
+//   }
+// })
